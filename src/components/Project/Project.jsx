c63352e0a9bdc607ff98436f4904076ed3d4feb6
Akash Bodhale <abodhale@Mac.cable.rcn.com>
2025-01-30 21:08:17 -0600

    added Project section

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -8,9 +8,24 @@ function Project()
             <div className="headingprojects">
                 <h1> MY Projects </h1>
                 <div className="project1">
-                    <div className="box1"></div>
-                    <div className="box1"></div>
-                    <div className="box1"></div>
+                    <div className="box1">
+                        <div className="androiddev">
+                            <h2>Sky Search AI</h2>
+                            <p>I create an android app called Sky search AI which is chatbot application. This is college project. I done this in subject ITMD-555. </p>
+                        </div>
+                    </div>
+                    <div className="box1">
+                        <div className="androiddev">
+                            <h2>Infant Appointment Systeme</h2>
+                            <p>I created an 3-tier web application using jsp and servlet for the database I used mysql.</p>
+                        </div>
+                    </div>
+                    <div className="box1">
+                        <div className="androiddev">
+                            <h2>Well architected cloud Infrostructure</h2>
+                            <p>I worked in this project in build fellowship program. I learn the concept of terraform and I learn how to architect the project and how to deploy it on AWS</p>
+                        </div>
+                    </div>
                 </div>
             </div> 
         </div>  
